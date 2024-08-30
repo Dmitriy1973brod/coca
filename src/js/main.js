7ab814ab116f9c5e8207bbe6dab52d26cfddcf81
Dmitriy <dmitrygritskevich@yandex.ru>
2024-08-30 18:51:40 +0300

    Created insight section and added swiper

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import '../assets/scss/main.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { insightSlider } from './components/sliger.js';
 
 useTheme();
 useBurger();
+insightSlider();

759bbbcf6d5ce3e687eb97e166c42009836fdeb1
Dmitriy <dmitrygritskevich@yandex.ru>
2024-09-06 16:09:59 +0300

    homeworks

--- src/js/components/home/sliger.js
+++ src/js/components/home/sliger.js
@@ -1,13 +1,27 @@
 import Swiper from 'swiper';
-import { Navigation} from 'swiper/modules';
+import { Autoplay, Navigation, Pagination, EffectFade } from 'swiper/modules';
 import 'swiper/css';
 
 export const insightSlider = () => {
   new Swiper('.insight__slider', {
+    modules: [Navigation, Pagination, EffectFade],
     slidesPerView: 'auto',
     spaceBetween: 32,
     loop: true,
     centeredSlides: true,
+    speed: 500,
+    // effect: 'fade',
+    // fadeEffect: {
+    //   crossFade: true,
+    // },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.insight__btn--next',
+      prevEl: '.insight__btn--prev',
+    },
     breakpoints: {
       992: {
         centeredSlides: false,
@@ -17,15 +31,37 @@ export const insightSlider = () => {
 };
 
 export const useTestimonialsSlider = () => {
-    new Swiper('.testimonials__slider', {
-        modules: [Navigation],
-        slidesPerView: 'auto',
-        spaceBetween: 22,
-        loop: true,
-        navigation: {
-            nextEl: '.testimonials__btn--next',
-            prevEl: '.testimonials__btn--prev',
-          },
-        
-    })
-}
+  new Swiper('.testimonials__slider', {
+    modules: [Navigation, EffectFade],
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: 22,
+    loop: true,
+    speed: 500,
+    // effect: 'fade',
+    // fadeEffect: {
+    //   crossFade: true,
+    // },
+    navigation: {
+      nextEl: '.testimonials__btn--next',
+      prevEl: '.testimonials__btn--prev',
+    },
+  });
+};
+
+export const autoParthersSlider = () => {
+  new Swiper('.partners__slider-mobile', {
+    modules: [Autoplay],
+    slidesPerView: 3,
+    spaceBetween: 0,
+    loop: true,
+    speed: 2000,
+    centeredSlides: true,
+    speed: 1000,
+    autoplay: {
+      delay: 500,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,10 @@ import '../assets/scss/main.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import { insightSlider, useTestimonialsSlider } from './components/home/sliger.js';
+import { insightSlider, useTestimonialsSlider, autoParthersSlider } from './components/home/sliger.js';
 
 useTheme();
 useBurger();
 insightSlider();
 useTestimonialsSlider();
+autoParthersSlider();

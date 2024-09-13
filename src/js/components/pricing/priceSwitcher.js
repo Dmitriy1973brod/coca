ecbe66dcd88409dddd27838dd37bba7f038b977f
Dmitriy <dmitrygritskevich@yandex.ru>
2024-09-13 20:33:23 +0300

    Created pricing: faq section

--- src/js/components/pricing/priceSwitcher.js
+++ src/js/components/pricing/priceSwitcher.js
@@ -4,18 +4,28 @@ export const usePriceSwitcher = () => {
   const popularPrice = document.querySelector('[data-price="popular"]');
   const enterprisePrice = document.querySelector('[data-price="enterprise"]');
 
+  const starterPriceTime = document.querySelector('[data-price="starter-time"]');
+  const popularPriceTime = document.querySelector('[data-price="popular-time"]');
+  const enterprisePriceTime = document.querySelector('[data-price="enterprise-time"]');
+
   const priceList = {
     starter: {
       default: 120,
-      witchSale: 100,
+      witchSale: 100*12,
+      timeDefault: '/mo',
+      time: '/yr',      
     },
     popular: {
       default: 1680,
-      witchSale: 1400,
+      witchSale: 1400*12,
+      timeDefault: '/mo',
+      time: '/yr',      
     },
     enterprise: {
       default: 2520,
-      witchSale: 2100,
+      witchSale: 2100*12,
+      timeDefault: '/mo',
+      time: '/yr',      
     },
   }; 
   
@@ -23,12 +33,20 @@ export const usePriceSwitcher = () => {
     starterPrice.textContent = priceList.starter.witchSale;
     popularPrice.textContent = priceList.popular.witchSale;
     enterprisePrice.textContent = priceList.enterprise.witchSale;
+
+    starterPriceTime.textContent = priceList.starter.time;
+    popularPriceTime.textContent = priceList.popular.time;
+    enterprisePriceTime.textContent = priceList.enterprise.time;
   };
 
   const setDefaultPrices = () => {
     starterPrice.textContent = priceList.starter.default;
     popularPrice.textContent = priceList.popular.default;
     enterprisePrice.textContent = priceList.enterprise.default;
+
+    starterPriceTime.textContent = priceList.starter.timeDefault;
+    popularPriceTime.textContent = priceList.popular.timeDefault;
+    enterprisePriceTime.textContent = priceList.enterprise.timeDefault;
   }
 
   switcher.checked = true;
--- src/js/pricing.js
+++ src/js/pricing.js
@@ -3,7 +3,9 @@ import '../assets/scss/pricing.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
+import { useFooter } from './components/home/footer.js';
 
 useTheme();
 useBurger();
+useFooter();
 usePriceSwitcher();

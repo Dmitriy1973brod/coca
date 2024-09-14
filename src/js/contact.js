b62a75c7372cf5dce1305d0f3f3f31c2971095cd
Dmitriy <dmitrygritskevich@yandex.ru>
2024-09-14 18:06:39 +0300

    Created contact:hero and added contact:partners

---
+++ src/js/contact.js
@@ -0,0 +1,9 @@
+import '../assets/scss/contact.scss';
+
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useFooter } from './components/home/footer.js';
+
+useTheme();
+useBurger();
+useFooter();

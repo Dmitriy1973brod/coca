acc686dd0f73bc886ec7c0bf9cd3117cf90d87cf
Dmitriy <dmitrygritskevich@yandex.ru>
2024-09-17 18:54:18 +0300

    Created contact:get-started section

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,8 +2,12 @@ import '../assets/scss/contact.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { autoParthersSlider } from './components/home/sliger.js';
+import { usePhone } from './components/contact/phone.js';
 import { useFooter } from './components/home/footer.js';
 
 useTheme();
 useBurger();
+autoParthersSlider();
+usePhone();
 useFooter();
--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,11 @@ import 'swiper/css/effect-fade';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import { insightSlider, useTestimonialsSlider, autoParthersSlider } from './components/home/sliger.js';
+import {
+  insightSlider,
+  useTestimonialsSlider,
+  autoParthersSlider,
+} from './components/home/sliger.js';
 import { useFooter } from './components/home/footer.js';
 
 useTheme();
